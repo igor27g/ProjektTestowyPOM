47954ceedb9491d2e66e7e76f7ad5da0b53e9dc9
Igor Grabowski <aigor2789@gmail.com>
2020-02-20 23:25:58 +0100

    Add selenium server

---
+++ Selenium Server/nodestart.bat
@@ -0,0 +1,1 @@
+java -jar selenium-server-standalone-3.141.59.jar -role webdriver -hub http://localhost:4444/grid/register -port 5556 -browser "browserName=iexplore-browser, maxInstances=1" -browser "browserName=firefox, maxInstances=4" -browser "browserName=chrome, maxInstances=3, version=80" -browser "browserName=MicrosoftEdge, maxInstances=1" -browser "browserName=operablink, maxInstances=3"
--- src/test/java/TestyPOM/BaseTest.java
+++ src/test/java/TestyPOM/BaseTest.java
@@ -2,11 +2,13 @@ package TestyPOM;
 import Drivers.DriverFactory;
 import Utils.ConfigurationReader;
 import Utils.TestDataReader;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInstance;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -28,10 +30,11 @@ public class BaseTest {
     @BeforeEach
     public void testSetUp(){
 
-        DriverFactory driverFactory = new DriverFactory();
-        driver = driverFactory.create(configuration);
-        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-
+//        DriverFactory driverFactory = new DriverFactory();
+//        driver = driverFactory.create(configuration);
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
         driver.manage().window().maximize();
     }
 
